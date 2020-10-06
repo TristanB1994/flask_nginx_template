dc8b9341995a9da350635f5f16d8521531ff02a8
TristanB1994 <t.blandford.dev@gmail.com>
2020-10-05 21:35:18 -0400

    added user auth

--- flask/app/views.py
+++ flask/app/views.py
@@ -1,9 +1,9 @@
 from app import app, admin
 from flask_sqlalchemy import SQLAlchemy
-from flask import render_template, request, url_for
-from flask_admin import BaseView, expose
+from flask import redirect, render_template, request, url_for
+from flask_admin import BaseView, expose, AdminIndexView
 from flask_admin.contrib.sqla import ModelView
-from flask_login import UserMixin
+from flask_login import UserMixin, LoginManager, current_user, login_user, logout_user
 
 # Configuration
 
@@ -12,13 +12,26 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///example.db'
 app.config['SECRET_KEY'] = 'mysecret'
 
 db = SQLAlchemy(app)
+login = LoginManager(app)
+
+@login.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(20))
 
 class MyModelView(ModelView):
-    pass
+    def is_accessible(self):
+        return current_user.is_authenticated
+
+    def inaccessible_callback(self, name, **kwargs):
+        return redirect(url_for('login'))
+
+class MyAdminIndexView(AdminIndexView):
+    def is_accessible(self):
+        return current_user.is_authenticated
 
 # Admin routes
 
@@ -32,3 +45,14 @@ admin.add_view(MyModelView(User, db.session))
 def home():
 
     return render_template("index.html")
+
+@app.route('/login')
+def login():
+    user = User.query.get(1)
+    login_user(user)
+    return 'Logged in'
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return "logged out!"
